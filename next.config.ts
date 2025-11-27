ad839fbe854640978b91319cba42a91b7324247b
Aaron Nolan <aaron.nolan24@gmail.com>
2025-11-26 18:11:13 -0600

    use env file for base path

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/trttrackerweb' : '',
+  basePath: isProd ? (process.env.BASE_PATH || '') : '',
   images: {
     unoptimized: true,
   },
